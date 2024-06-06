b50c6d85f974867047390457ceac20ea65b52dd9
Thomas Stuart <tjstuart@google.com>
2024-06-06 22:34:33 +0000

    enforce the calling package is the default dialer for VisualVoicemail
    In the docs for setVisualVoicemailSmsFilterSettings, they state that
    the callingPackage needs to be the default dialer. However, server side,
    there is no enforcement for this. Now, every client is verified to be
    the default dialer, system dialer, or carrier visual voicemail app
    before changing the settings for visual voicemail.
    
    Bug: 308932906
    Test: CTS
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c27f06d3401d8a0caf93ee539d67e687ff429f3b)
    
    Change-Id: I9f0f35253e13131019c779e137ebb224e4872882

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -4281,9 +4281,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     public void enableVisualVoicemailSmsFilter(String callingPackage, int subId,
             VisualVoicemailSmsFilterSettings settings) {
         mAppOps.checkPackage(Binder.getCallingUid(), callingPackage);
-        enforceVisualVoicemailPackage(callingPackage, subId);
         enforceTelephonyFeatureWithException(callingPackage,
                 PackageManager.FEATURE_TELEPHONY_CALLING, "enableVisualVoicemailSmsFilter");
+        enforceVisualVoicemailPackage(callingPackage, subId);
         final long identity = Binder.clearCallingIdentity();
         try {
             VisualVoicemailSmsFilterConfig.enableVisualVoicemailSmsFilter(
@@ -4296,10 +4296,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     @Override
     public void disableVisualVoicemailSmsFilter(String callingPackage, int subId) {
         mAppOps.checkPackage(Binder.getCallingUid(), callingPackage);
-        enforceVisualVoicemailPackage(callingPackage, subId);
         enforceTelephonyFeatureWithException(callingPackage,
                 PackageManager.FEATURE_TELEPHONY_CALLING, "disableVisualVoicemailSmsFilter");
-
+        enforceVisualVoicemailPackage(callingPackage, subId);
         final long identity = Binder.clearCallingIdentity();
         try {
             VisualVoicemailSmsFilterConfig.disableVisualVoicemailSmsFilter(
